0aaaae68801fdd22c2787d3060653a91f7b4111b
Rasalas Chow <rasalaschow@foxmail.com>
2020-11-17 23:53:04 +0800

    Week 1 finished

--- week1/week1_4.py
+++ week1/week1_4.py
@@ -15,27 +15,21 @@ from tensorflow.keras.preprocessing.sequence import pad_sequence
 #     https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json \
 #     -O /tmp/sarcasm.json
 
-def import_dataset():
-    with open("/tmp/sarcasm.json", 'r') as f:
-        datastore = json.load(f)
 
-    sentences = []
-    labels = []
-    urls = []
-    for item in datastore:
-        sentences.append(item['headline'])
-        labels.append(item['is_sarcastic'])
-        urls.append(item['article_link'])
+with open("/archive/sarcasm.json", 'r') as f:
+    datastore = json.load(f)
 
-def get_tokenizer(corpus):
-    tokenizer = Tokenizer(oov_token='<OOV>')
-    tokenizer.fit_on_texts(sentences)
-    return tokenizer
+sentences = []
+labels = []
+urls = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
 
-def get_word2idx(tokenizer):
-    return tokenizer.word_index
+tokenizer = Tokenizer(oov_token='<OOV>')
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
 
-def get_padded(tokenizer, sentences):
-    sequences = tokenizer.texts_to_sequence(sentences)
-    padded = pad_sequence(seqences, padding='post')
-    return padded
+sequences = tokenizer.texts_to_sequence(sentences)
+padded = pad_sequence(seqences, padding='post')
